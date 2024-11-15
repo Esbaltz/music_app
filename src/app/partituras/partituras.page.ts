a7a6cc7ff1d6aa630a155c21578a02abb850dcfa
Elías <el.antican@duocuc.cl>
2024-11-15 10:13:33 -0300

    notas_botones

--- src/app/partituras/partituras.page.ts
+++ src/app/partituras/partituras.page.ts
@@ -6,45 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./partituras.page.scss'],
 })
 export class PartiturasPage {
-  respuesta: string = '';
   mensaje: string = '';
   notaActual: { nombre: string; cifradoAmericano: string; imagen: string } | null = null;
 
-  // Arreglo de notas con nombres y cifrado americano
+  // Para el contador
+  totalNotas: number = -1; 
+  notasAdivinadas: number = 0; 
+
+  // Arreglo de notas con cifrados americano y tradicional
   notas = [
-    { nombre: 'Do', cifradoAmericano: 'C', imagen: 'assets/img/partituras/C.jpg' },
-    { nombre: 'Re', cifradoAmericano: 'D', imagen: 'assets/img/partituras/D.jpg' },
-    { nombre: 'Mi', cifradoAmericano: 'E', imagen: 'assets/img/partituras/E.jpeg' },
-    { nombre: 'Fa', cifradoAmericano: 'F', imagen: 'assets/img/partituras/F.jpg' },
-    { nombre: 'Sol', cifradoAmericano: 'G', imagen: 'assets/img/partituras/G.png' },
-    { nombre: 'La', cifradoAmericano: 'A', imagen: 'assets/img/partituras/A.png' },
-    { nombre: 'Si', cifradoAmericano: 'B', imagen: 'assets/img/partituras/B.webp' },
+    { nombre: 'Do', cifradoAmericano: 'C', imagen: 'assets/img/partituras/C.PNG' },
+    { nombre: 'Re', cifradoAmericano: 'D', imagen: 'assets/img/partituras/D.PNG' },
+    { nombre: 'Mi', cifradoAmericano: 'E', imagen: 'assets/img/partituras/E.PNG' },
+    { nombre: 'Fa', cifradoAmericano: 'F', imagen: 'assets/img/partituras/F.PNG' },
+    { nombre: 'Sol', cifradoAmericano: 'G', imagen: 'assets/img/partituras/G.PNG' },
+    { nombre: 'La', cifradoAmericano: 'A', imagen: 'assets/img/partituras/A.PNG' },
+    { nombre: 'Si', cifradoAmericano: 'B', imagen: 'assets/img/partituras/B.PNG' },
   ];
 
   constructor() {
-    this.seleccionarNotaAleatoria(); // Asignar una nota aleatoria al iniciar
+    this.seleccionarNotaAleatoria();
   }
 
   // Seleccionar una nota aleatoria
   seleccionarNotaAleatoria() {
     const indiceAleatorio = Math.floor(Math.random() * this.notas.length);
     this.notaActual = this.notas[indiceAleatorio];
+    this.totalNotas++;
   }
 
-  // Verificar respuesta y seleccionar una nueva nota si es correcta
-  verificarRespuesta() {
-    const respuestaUsuario = this.respuesta.trim().toLowerCase();
+  // Verificar respuesta
+  verificarRespuesta(respuestaUsuario: string) {
     const esCorrecta =
-      respuestaUsuario === this.notaActual?.nombre.toLowerCase() ||
-      respuestaUsuario === this.notaActual?.cifradoAmericano.toLowerCase();
+      respuestaUsuario.toLowerCase() === this.notaActual?.cifradoAmericano.toLowerCase();
 
     if (esCorrecta) {
       this.mensaje = '¡Correcto!';
+      this.notasAdivinadas++;
       this.seleccionarNotaAleatoria();
     } else {
       this.mensaje = 'Intenta de nuevo.';
+      this.totalNotas++;
     }
-
-    this.respuesta = ''; // Limpiar el campo de respuesta
   }
 }
