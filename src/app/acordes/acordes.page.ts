e5b4a8f8a9c46bbc1475f5dab2df8b6690e350ae
Elías <el.antican@duocuc.cl>
2024-11-15 10:59:21 -0300

    acordes

--- src/app/acordes/acordes.page.ts
+++ src/app/acordes/acordes.page.ts
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./acordes.page.scss'],
 })
 export class AcordesPage implements OnInit {
+  mensaje: string = '';
+  acordeActual: { nombre: string; variantes: string[]; imagen: string } | null = null;
 
-  constructor() { }
+  acordes = [
+    { nombre: 'Do', variantes: ['C'], imagen: 'assets/img/acordes/C_chord.PNG' },
+  ];
+
+  constructor() {
+    this.seleccionarAcordeAleatorio();
+   }
 
   ngOnInit() {
+  }
+
+  seleccionarAcordeAleatorio() {
+    const indiceAleatorio = Math.floor(Math.random() * this.acordes.length);
+    this.acordeActual = this.acordes[indiceAleatorio];
+  }
+
+  verificarRespuesta(respuestaUsuario: string) {
+    const esCorrecta = this.acordeActual?.variantes.includes(respuestaUsuario);
+
+    if (esCorrecta) {
+      this.mensaje = '¡Correcto!';
+      this.seleccionarAcordeAleatorio();
+    } else {
+      this.mensaje = 'Intenta de nuevo.';
+    }
+  }
+
+  getCifradosUnicos(): string[] {
+    // Crear una lista de todos los cifrados (tomando la primera variante como representativa)
+    const cifrados = this.acordes.map((nota) => nota.variantes[0]);
+    
+    // Eliminar duplicados usando Set
+    return [...new Set(cifrados)];
   }
 
 }
--- src/app/partituras/partituras.page.ts
+++ src/app/partituras/partituras.page.ts
@@ -7,38 +7,36 @@ import { Component } from '@angular/core';
 })
 export class PartiturasPage {
   mensaje: string = '';
-  notaActual: { nombre: string; cifradoAmericano: string; imagen: string } | null = null;
+  notaActual: { nombre: string; variantes: string[]; imagen: string } | null = null;
 
-  // Para el contador
-  totalNotas: number = -1; 
-  notasAdivinadas: number = 0; 
+  totalNotas: number = 0;
+  notasAdivinadas: number = 0;
 
-  // Arreglo de notas con cifrados americano y tradicional
   notas = [
-    { nombre: 'Do', cifradoAmericano: 'C', imagen: 'assets/img/partituras/C.PNG' },
-    { nombre: 'Re', cifradoAmericano: 'D', imagen: 'assets/img/partituras/D.PNG' },
-    { nombre: 'Mi', cifradoAmericano: 'E', imagen: 'assets/img/partituras/E.PNG' },
-    { nombre: 'Fa', cifradoAmericano: 'F', imagen: 'assets/img/partituras/F.PNG' },
-    { nombre: 'Sol', cifradoAmericano: 'G', imagen: 'assets/img/partituras/G.PNG' },
-    { nombre: 'La', cifradoAmericano: 'A', imagen: 'assets/img/partituras/A.PNG' },
-    { nombre: 'Si', cifradoAmericano: 'B', imagen: 'assets/img/partituras/B.PNG' },
+    { nombre: 'Do', variantes: ['C'], imagen: 'assets/img/partituras/C.PNG' },
+    { nombre: 'Re', variantes: ['D'], imagen: 'assets/img/partituras/D.PNG' },
+    { nombre: 'Mi', variantes: ['E'], imagen: 'assets/img/partituras/E.PNG' },
+    { nombre: 'Fa', variantes: ['F'], imagen: 'assets/img/partituras/F.PNG' },
+    { nombre: 'Sol', variantes: ['G'], imagen: 'assets/img/partituras/G.PNG' },
+    { nombre: 'La', variantes: ['A'], imagen: 'assets/img/partituras/A.PNG' },
+    { nombre: 'La agudo', variantes: ['A'], imagen: 'assets/img/partituras/A2.PNG' },
+    { nombre: 'Si', variantes: ['B'], imagen: 'assets/img/partituras/B.PNG' },
   ];
+
+  notasSostenidas = ['C#', 'D#', 'F#', 'G#', 'A#'];
 
   constructor() {
     this.seleccionarNotaAleatoria();
   }
 
-  // Seleccionar una nota aleatoria
   seleccionarNotaAleatoria() {
     const indiceAleatorio = Math.floor(Math.random() * this.notas.length);
     this.notaActual = this.notas[indiceAleatorio];
     this.totalNotas++;
   }
 
-  // Verificar respuesta
   verificarRespuesta(respuestaUsuario: string) {
-    const esCorrecta =
-      respuestaUsuario.toLowerCase() === this.notaActual?.cifradoAmericano.toLowerCase();
+    const esCorrecta = this.notaActual?.variantes.includes(respuestaUsuario);
 
     if (esCorrecta) {
       this.mensaje = '¡Correcto!';
@@ -46,7 +44,14 @@ export class PartiturasPage {
       this.seleccionarNotaAleatoria();
     } else {
       this.mensaje = 'Intenta de nuevo.';
-      this.totalNotas++;
     }
+  }
+
+  getCifradosUnicos(): string[] {
+    // Crear una lista de todos los cifrados (tomando la primera variante como representativa)
+    const cifrados = this.notas.map((nota) => nota.variantes[0]);
+    
+    // Eliminar duplicados usando Set
+    return [...new Set(cifrados)];
   }
 }
